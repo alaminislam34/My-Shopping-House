38cebff8eebcbc0dd6e55e502338c93067e7a660
alaminislam34 <mi3548514@gmail.com>
2025-02-13 17:13:27 +0600

    added my first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold text-red-500">Hello world</h1>
+        <button className="btn">button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../Pages/Shared/Navbar/Navbar";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default MainLayout;
