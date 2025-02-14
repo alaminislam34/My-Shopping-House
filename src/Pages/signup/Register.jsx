200ade84af1eb9a8aebe0a3219d9252aa32648a2
Your Name <your-email@example.com>
2025-02-14 20:15:56 +0600

    added login and register functionality

--- src/Pages/signup/Register.jsx
+++ src/Pages/signup/Register.jsx
@@ -1,47 +1,108 @@
 import { FaGoogle } from "react-icons/fa";
 import image from "../../assets/register.png";
 import { Link } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import { imageUpload } from "../../api/utils";
+import Swal from "sweetalert2";
+
 const Register = () => {
+  const { createUser } = useAuth();
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const image = form.image.files[0];
+
+    // Validate password
+    const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{6,}$/;
+    if (!regex.test(password)) {
+      Swal.fire({
+        title: "Invalid Password",
+        text: "Use at least one uppercase, one lowercase, and one digit.",
+        icon: "warning",
+        confirmButtonText: "OK",
+      });
+      return;
+    }
+
+    try {
+      const photoURL = await imageUpload(image);
+
+      // Create new user
+      const res = await createUser(email, password);
+      console.log(res);
+
+      Swal.fire({
+        title: "Signup Successful",
+        icon: "success",
+        confirmButtonText: "OK",
+      });
+    } catch (err) {
+      console.error(err);
+      Swal.fire({
+        title: "Error",
+        text: err?.message || "Something went wrong!",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+    }
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col-reverse lg:flex-row-reverse">
-        {/* right side  */}
+        {/* Right Side */}
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form onSubmit={handleRegister} className="card-body">
             <h2 className="text-black font-bold text-2xl">Create an account</h2>
             <p className="text-base text-gray-400 font-light">
               Enter Your Details Below
             </p>
 
             <div className="form-control">
-              <label className="label">
-                {/* <span className="label-text">Name</span> */}
-              </label>
               <input
+                name="name"
                 type="text"
                 placeholder="Name"
                 className="input -ml-3 input-bordered border-x-0 border-t-0 rounded-none focus:ring-0 focus:outline-none"
                 required
               />
             </div>
+
+            {/* Image Upload */}
             <div className="form-control">
-              <label className="label">
-                {/* <span className="label-text">Email</span> */}
+              <label htmlFor="image" className="block text-sm mb-2">
+                Select Image:
               </label>
               <input
+                required
+                type="file"
+                id="image"
+                name="image"
+                className="file-input file-input-bordered w-full -ml-3 rounded-none input-bordered border-x-0 border-t-0 focus:ring-0 focus:outline-none"
+                accept="image/*"
+              />
+            </div>
+
+            <div className="form-control">
+              <input
+                name="email"
                 type="email"
-                placeholder="email"
+                placeholder="Email"
                 className="input -ml-3 rounded-none input-bordered border-x-0 border-t-0 focus:ring-0 focus:outline-none"
                 required
               />
             </div>
+
             <div className="form-control">
-              <label className="label">
-                {/* <span className="label-text">Password</span> */}
-              </label>
               <input
+                name="password"
                 type="password"
-                placeholder="password"
+                placeholder="Password"
                 className="input -ml-3 input-bordered rounded-none border-x-0 border-t-0 focus:ring-0 focus:outline-none"
                 required
               />
@@ -51,12 +112,16 @@ const Register = () => {
                 </a>
               </label>
             </div>
+
             <div className="form-control mt-6">
               <button className="btn text-white w-full bg-[#db4444]">
                 Create Account
               </button>
-              <button className="btn mt-1 text-black w-full bg-white">
-                <FaGoogle></FaGoogle> Sign up with Google
+              <button
+                type="button"
+                className="btn mt-1 text-black w-full bg-white"
+              >
+                <FaGoogle /> Sign up with Google
               </button>
               <p className="text-base text-gray-600 mt-3 font-light">
                 Already Have an account?{" "}
@@ -68,9 +133,9 @@ const Register = () => {
           </form>
         </div>
 
-        {/* left side */}
+        {/* Left Side */}
         <div className="text-center lg:text-left">
-          <img className="h-[600px]" src={image} alt="" />
+          <img className="h-[600px]" src={image} alt="Register" />
         </div>
       </div>
     </div>
